240daeb074e0cadb2b59dfebc1bd6d8e8acc7dad
Marcus Connor <marcusconnor@users.noreply.github.com>
2015-06-10 15:49:39 -0400

    Added the fisherman words.

--- worm.php
+++ worm.php
@@ -3,8 +3,13 @@
 // The worm says
 $worm_words = "Hello y'all!";
 
+// The fisherman says
+$fisherman_words = "Hello, you look like bait.";
+
 echo $worm_words;
 
 echo "<br>";
+
+echo $fisherman_words;
 
 ?>
