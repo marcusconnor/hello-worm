d65159f41bce5f2e6d3196380d231dd2e2d88cac
Marcus Connor <marcusconnor@users.noreply.github.com>
2017-02-02 18:23:21 -0500

    Added HTML tags Isn't that great?

--- worm.php
+++ worm.php
@@ -6,10 +6,14 @@ $worm_words = "Hello y'all!";
 // The fisherman says
 $fisherman_words = "Hello, you look like bait.";
 
+echo "<html>";
+
 echo $worm_words;
 
 echo "<br>";
 
 echo $fisherman_words;
+
+echo "</html>";
 
 ?>
